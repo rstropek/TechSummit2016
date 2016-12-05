79c8ad689f777e1e3df47a1ffddefff837fc0c67
Rainer Stropek <rainer@software-architects.at>
2016-12-05 16:58:50 +0100

    .

--- Shop.Client/build-test.sh
+++ Shop.Client/build-test.sh
@@ -1,5 +1,5 @@
 pwd
 ls -la
 docker run --rm -v $(pwd)/Shop.Client:/app -w="/app" node:6.9.1 /bin/bash -c "ls -la"
-docker run --rm -v $(pwd):/app -w="/app" node:6.9.1 /bin/bash -c "pwd"
+docker run --rm -v $(pwd):/app -w="/app" node:6.9.1 /bin/bash -c "ls -la"
 
